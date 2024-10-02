bab9f623cb3e789f6cfe0fd680bdd8020f29f4d6
JulieDelts <juliadelcova@gmail.com>
2024-10-02 15:28:50 +0300

    Create Core BaseProduct input and output models and an delete empty class

---
+++ InventoryCheckPlatform.Core/OutputModels/BasePoductOutputModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace InventoryCheckPlatform.Core.OutputModels
+{
+	public class BasePoductOutputModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Category { get; set; }
+	}
+}
