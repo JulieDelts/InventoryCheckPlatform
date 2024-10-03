9401f340e1022a0fdcd34ba6e5a43bff689a99d0
Питенко София <pitenko.sofiya@mail.ru>
2024-10-03 23:26:51 +0300

    AddFullOutputModel

---
+++ InventoryCheckPlatform.Core/OutputModels/FullUserOutputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryCheckPlatform.Core.OutputModels
+{
+    public class FullUserOutputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string UserRole { get; set; }
+        public int RestaurantId { get; set; }
+
+    }
+}
