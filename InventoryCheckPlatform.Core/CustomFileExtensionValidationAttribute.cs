30643b110df02bb7d8ffc95f921b6feaedd511d0
JulieDelts <juliadelcova@gmail.com>
2024-10-08 20:07:42 +0300

    Add image validation attribute

---
+++ InventoryCheckPlatform.Core/CustomFileExtensionValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryCheckPlatform.Core
+{
+    public class CustomFileExtensionValidationAttribute: ValidationAttribute
+    {
+        List<string> _validFileExtensions = new() { "jpg", "jpeg", "bmp", "png" };
+
+        public CustomFileExtensionValidationAttribute()
+        {
+            ErrorMessage = "Я картинки хочу, кретин!";
+        }
+        public override bool IsValid(object? fileName)
+        {
+            if (fileName == null) return false;
+
+            var file = (string)fileName;
+
+            if (string.IsNullOrEmpty(file)) return false;
+
+            var extension = file.Split(".").Last().ToLower();
+
+            return _validFileExtensions.Contains(extension);
+        }
+    }
+}
