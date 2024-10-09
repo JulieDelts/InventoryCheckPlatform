b02b3966ea4360ae70a8ce90f25ed5a708ce102e
JulieDelts <juliadelcova@gmail.com>
2024-10-09 23:22:14 +0300

    Create DTOs

---
+++ InventoryCheckPlatform.Core/DTOs/ShippingDocument.cs
@@ -0,0 +1,16 @@
+﻿
+namespace InventoryCheckPlatform.Core.DTOs
+{
+	public class ShippingDocument
+	{
+		public int Id { get; set; }
+
+		public User Logistician { get; set; }
+
+		public Restaurant Restaurant { get; set; }
+
+		public List<SpecificProduct> Products { get; set; }
+
+		public double FullPrice { get; set; }
+	}
+}
