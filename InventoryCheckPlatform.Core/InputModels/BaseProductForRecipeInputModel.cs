d9c69ee5ec0c24a4086344bf1c2c6c487e02c469
Питенко София <pitenko.sofiya@mail.ru>
2024-10-18 03:04:48 +0300

    Refresh recipe pages

---
+++ InventoryCheckPlatform.Core/InputModels/BaseProductForRecipeInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryCheckPlatform.Core.InputModels
+{
+    public class BaseProductForRecipeInputModel
+    {
+        [Required(ErrorMessage = "Необходимо ввести название.")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Count { get; set; }  
+
+    }
+}
--- InventoryCheckPlatform.Core/InputModels/BaseProductInputModel.cs
+++ InventoryCheckPlatform.Core/InputModels/BaseProductInputModel.cs
@@ -11,5 +11,6 @@ namespace InventoryCheckPlatform.Core.InputModels
 
         [CustomFileExtensionValidation]
         public string? FileName { get; set; }
+
     }
 }
